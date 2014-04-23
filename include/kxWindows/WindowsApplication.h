0344b4eac4bcd372b6eb42bacc7fee359893b256
krmo.CORE <kristian.mostrom@gmail.com>
2014-04-24 01:17:49 +0200

    exposed window class name parameter in base class

--- include/kxWindows/WindowsApplication.h
+++ include/kxWindows/WindowsApplication.h
@@ -15,7 +15,7 @@ public:
 	WindowsApplication();
 	virtual ~WindowsApplication() {}
 
-	virtual void CreateWindows(const char* title, int width, int height);
+	virtual void CreateWindows(const char* windowClassName, const char* title, int width, int height);
 	virtual int MessageLoop();
 
 	static LRESULT CALLBACK stWndProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
